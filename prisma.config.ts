13d60e6462918aa35f0c8a8fc9e4c187ca08a9e6
Shiota Zero <ryukokaru@gmail.com>
2026-05-14 23:13:43 +0700

    fix:tsconfig

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DIRECT_URL"]
-    // directUrl : process.env["DIRECT_URL"],
+    url: process.env["DATABASE_URL"],
+    directUrl : process.env["DIRECT_URL"],
   },
 });
