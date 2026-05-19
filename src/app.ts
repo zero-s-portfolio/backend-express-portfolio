a253218189a586e42f62a0461fd9b4735e085c24
Shiota Zero <ryukokaru@gmail.com>
2026-05-19 09:24:49 +0700

    update cors origin

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://app.apidog.com"],
+    origin: ["http://localhost:5173", "https://app.apidog.com", "https://zero-sensei.my.id"],
     credentials: true,
   })
 );
