59c5be7e060070649281da835b7dbffcbaf659d1
Shiota Zero <ryukokaru@gmail.com>
2026-05-19 13:16:30 +0700

    update cors origin

--- src/validations/contact.validation.ts
+++ src/validations/contact.validation.ts
@@ -1,12 +1,48 @@
 import { z } from "zod";
 
 export const createContactMessageSchema = z.object({
-  name: z.string().min(2).optional(),
-  email: z.string().email(),
-  subject: z.string().optional(),
-  message: z.string().min(10),
+  name: z
+    .string({
+      error: "Nama wajib diisi",
+    })
+    .min(2, {
+      message: "Nama minimal 2 karakter",
+    }),
+
+  email: z
+    .string({
+      error: "Email wajib diisi",
+    })
+    .email({
+      message: "Format email tidak valid",
+    }),
+
+  subject: z
+    .string({
+      error: "Subject wajib diisi",
+    })
+    .min(3, {
+      message: "Subject minimal 3 karakter",
+    }),
+
+  message: z
+    .string({
+      error: "Pesan wajib diisi",
+    })
+    .min(10, {
+      message: "Pesan minimal 10 karakter",
+    }),
+
   category: z
-    .enum(["GENERAL", "FREELANCE", "COLLABORATION", "CONSULTATION", "BUG"])
+    .enum([
+      "GENERAL",
+      "FREELANCE",
+      "COLLABORATION",
+      "CONSULTATION",
+      "BUG",
+    ], {
+      error: "Kategori tidak valid",
+    })
     .optional(),
 });
 
